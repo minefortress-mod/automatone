53b79028eba8cf86c48b5073fcd46548941fbf6f
Alexandr <lwjgl@mail.ru>
2022-08-02 23:41:28 +0200

    Instead of cancel AI cacel each control

--- src/launch/java/baritone/launch/mixins/MixinJumpControl.java
+++ src/launch/java/baritone/launch/mixins/MixinJumpControl.java
@@ -18,31 +18,28 @@
 package baritone.launch.mixins;
 
 import baritone.BaritoneProvider;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.attribute.EntityAttributes;
+import baritone.api.minefortress.IMinefortressEntity;
+import net.minecraft.entity.ai.control.Control;
+import net.minecraft.entity.ai.control.JumpControl;
 import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MobEntity.class)
-public abstract class MixinMobEntity extends LivingEntity {
-    @Shadow public abstract void setMovementSpeed(float movementSpeed);
+@Mixin(JumpControl.class)
+public abstract class MixinJumpControl implements Control {
 
-    protected MixinMobEntity(EntityType<? extends LivingEntity> entityType, World world) {
-        super(entityType, world);
-    }
+    @Shadow @Final private MobEntity entity;
 
-    @Inject(method = "tickNewAi", at = @At("HEAD"), cancellable = true)
-    private void cancelAiTick(CallbackInfo ci) {
-        if (BaritoneProvider.INSTANCE.isPathing(this)) {
-            // mobs tend to set their movement speed to 0, preventing any movement
-            this.setMovementSpeed((float) this.getAttributeValue(EntityAttributes.GENERIC_MOVEMENT_SPEED));
-//            ci.cancel();
+    @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
+    void tick(CallbackInfo ci) {
+        final var entity = this.entity;
+        if(entity instanceof IMinefortressEntity && BaritoneProvider.INSTANCE.isPathing(entity)) {
+            ci.cancel();
         }
     }
+
 }
--- src/launch/java/baritone/launch/mixins/MixinLookControl.java
+++ src/launch/java/baritone/launch/mixins/MixinLookControl.java
@@ -18,31 +18,28 @@
 package baritone.launch.mixins;
 
 import baritone.BaritoneProvider;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.attribute.EntityAttributes;
+import baritone.api.minefortress.IMinefortressEntity;
+import net.minecraft.entity.ai.control.Control;
+import net.minecraft.entity.ai.control.LookControl;
 import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MobEntity.class)
-public abstract class MixinMobEntity extends LivingEntity {
-    @Shadow public abstract void setMovementSpeed(float movementSpeed);
+@Mixin(LookControl.class)
+public abstract class MixinLookControl implements Control {
 
-    protected MixinMobEntity(EntityType<? extends LivingEntity> entityType, World world) {
-        super(entityType, world);
-    }
+    @Shadow @Final protected MobEntity entity;
 
-    @Inject(method = "tickNewAi", at = @At("HEAD"), cancellable = true)
-    private void cancelAiTick(CallbackInfo ci) {
-        if (BaritoneProvider.INSTANCE.isPathing(this)) {
-            // mobs tend to set their movement speed to 0, preventing any movement
-            this.setMovementSpeed((float) this.getAttributeValue(EntityAttributes.GENERIC_MOVEMENT_SPEED));
-//            ci.cancel();
+    @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
+    void tick(CallbackInfo ci) {
+        final var entity = this.entity;
+        if (entity instanceof IMinefortressEntity && BaritoneProvider.INSTANCE.isPathing(entity)) {
+            ci.cancel();
         }
     }
+
 }
